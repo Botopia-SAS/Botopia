3055bc228c37fd706c6f96da32a85272e29e0a4a
Botopia-SAS <botopia.sas@gmail.com>
2025-08-05 21:22:37 -0500

    Integración de Microsoft clarity

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,6 +5,7 @@ import IntlProvider from "@/components/IntlProvider";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import { ThemeProvider } from "next-themes";
 import HeaderWrapper from "@/components/header/HeaderWrapper";
+import Script from "next/script";
 
 export const metadata = {
   title: "Botopia",
@@ -24,11 +25,22 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale} suppressHydrationWarning>
+      <head>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+          `}
+        </Script>
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <IntlProvider locale={locale} messages={messages}>
             <HeaderWrapper />
-            <main>{children}</main> 
+            <main>{children}</main>
             <WhatsAppButton />
           </IntlProvider>
         </ThemeProvider>
